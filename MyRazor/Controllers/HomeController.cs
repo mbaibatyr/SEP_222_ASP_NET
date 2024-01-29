b5b721cc297915bc24a3399653ffe4dfa9f7bf12
mbaibatyr <murat_b@mail.ru>
2024-01-29 21:44:10 +0600

    --

---
+++ MyRazor/Controllers/HomeController.cs
@@ -0,0 +1,28 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MyRazor.Models;
+using System.Diagnostics;
+
+namespace MyRazor.Controllers
+{
+    public class HomeController : Controller
+    {       
+        
+        public IActionResult Index()
+        {
+            ViewData["param1"] = "hello step";
+            return View();
+        }
+
+        public IActionResult Index2()
+        {            
+            return Content("1234");
+        }
+
+        [HttpPost]
+        public ActionResult MyPost(City city)
+        {
+            return Content(city.Id + " " + city.Name);
+        }
+
+    }
+}
---
+++ MyRazor/Models/City.cs
@@ -0,0 +1,8 @@
+﻿namespace MyRazor.Models
+{
+    public class City
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }    
+    }
+}
