7257212f6a117fe7c2237e47a99d89d69ec551cf
mbaibatyr <murat_b@mail.ru>
2024-01-31 19:17:13 +0600

    --

---
+++ MyRazor/Models/Photo.cs
@@ -0,0 +1,12 @@
+﻿namespace MyRazor.Models
+{
+    public class Photo
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string Extension { get; set; }
+        public int Width { get; set; }
+        public int Height { get; set; }
+        public DateTime DateCreated { get; set; }
+    }
+}
