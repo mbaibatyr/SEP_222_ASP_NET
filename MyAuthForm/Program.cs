3ed996cd641e55b51f74b6d43f90a3399a8e0a14
mbaibatyr <murat_b@mail.ru>
2024-02-12 21:43:06 +0600

    --

--- MyAuthForm/Program.cs
+++ MyAuthForm/Program.cs
@@ -9,8 +9,16 @@ namespace MyAuthForm
             var builder = WebApplication.CreateBuilder(args);
 
             builder.Services.AddAuthentication
-                (CookieAuthenticationDefaults.AuthenticationScheme).
-                    AddCookie(o => o.LoginPath = new PathString("/Account/Login"));
+                (CookieAuthenticationDefaults.AuthenticationScheme)
+                    //AddCookie(o => o.LoginPath = new PathString("/Account/Login"));
+                    .AddCookie(options =>
+                    {
+                        options.Cookie.Name = "MyIdentity";
+                        options.ExpireTimeSpan = TimeSpan.FromHours(8);
+                        options.SlidingExpiration = true;
+                        options.Cookie.MaxAge = TimeSpan.FromHours(8);
+                        options.LoginPath = new PathString("/Account/Login");
+                    });
 
             builder.Services.AddControllersWithViews();
 
