ca920cf2e73add25dfc19fcec19b3598911c3855
mbaibatyr <murat_b@mail.ru>
2024-03-06 21:43:35 +0500

    --

--- MyWebAPIBasicAuth/Controllers/TestController.cs
+++ MyWebAPIBasicAuth/Controllers/TestController.cs
@@ -7,7 +7,7 @@ namespace MyWebAPIBasicAuth.Controllers
 {
     [Route("[controller]")]
     [ApiController]
-    [Authorize]
+    //[Authorize]
 
     public class TestController : ControllerBase
     {
@@ -17,10 +17,32 @@ namespace MyWebAPIBasicAuth.Controllers
             return Ok($"Hello {name} {User.Identity.Name}  {User.FindFirstValue("psw")}");
         }
 
-        [HttpGet("Download")]
-        public ActionResult Download()
-        {
-            return Ok($"Hello {name} {User.Identity.Name}  {User.FindFirstValue("psw")}");
+
+        [HttpGet("set_cookie/{key}/{value}"), AllowAnonymous]
+        public ActionResult set_cookie(string key, string value)
+        {            
+            Response.Cookies.Append(key, value, new CookieOptions()
+            {
+                Expires = DateTimeOffset.Now.AddDays(12),
+                HttpOnly = true,
+                Secure = true                
+            });
+            return Ok("ok");
         }
+
+        [HttpGet("get_cookie/{key}"), AllowAnonymous]
+        public ActionResult get_cookie(string key)
+        {
+            var value = Request.Cookies[key];
+            return Ok(value);
+        }
+
+
+        //[HttpGet("Download")]
+
+        //public ActionResult Download()
+        //{
+        //    return Ok($"Hello {name} {User.Identity.Name}  {User.FindFirstValue("psw")}");
+        //}
     }
 }
