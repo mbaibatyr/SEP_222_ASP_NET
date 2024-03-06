8ec8999e258cbbbac8a2429d1d61fdf0af2d446d
mbaibatyr <murat_b@mail.ru>
2024-03-06 19:34:07 +0500

    --

---
+++ DapperToObjects/Model/country.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DapperToObjects.Model
+{
+    public class country
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public ICollection city { get; set; }
+
+    }
+
+    public class city
+    {
+        public int id { get; set; }
+        public string name { get; set; }
+        public int country_id { get; set; }
+    }
+}
