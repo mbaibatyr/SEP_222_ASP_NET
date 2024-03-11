14373a6793ab4a8bdae019ce22e9b7c60b6b29f3
mbaibatyr <murat_b@mail.ru>
2024-03-11 20:21:40 +0400

    --

--- MyWebAPI_JWT/Controllers/ValidateController.cs
+++ MyWebAPI_JWT/Controllers/ValidateController.cs
@@ -1,6 +1,11 @@
 ﻿using Microsoft.AspNetCore.Authorization;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
+using MyWebAPI_JWT.Model;
+using System.IdentityModel.Tokens.Jwt;
+using System.Security.Claims;
+using System.Text;
 
 namespace MyWebAPI_JWT.Controllers
 {
@@ -17,7 +22,47 @@ namespace MyWebAPI_JWT.Controllers
         [HttpPost, Route("GetToken")]
         public ActionResult GetToken(UserModel model)
         {
+            try
+            {
 
+
+                /*
+                 проверка в БД
+
+                 */
+
+                var securityKey = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["Jwt:Key"]));
+                var credentials = new SigningCredentials(securityKey, SecurityAlgorithms.HmacSha256);
+
+                var claims = new[] {
+                      new Claim("role", "admin"),
+                      new Claim("dateBirth", "2000-01-01")
+            };
+
+                var token = new JwtSecurityToken(config["Jwt:Issuer"],
+                    config["Jwt:Issuer"],
+                    claims,
+                    //null,
+                    expires: DateTime.Now.AddMinutes(5),
+                    signingCredentials: credentials);
+
+                var sToken = new JwtSecurityTokenHandler().WriteToken(token);
+
+                return Ok(new ReturnStatus
+                {
+                    status = StatusEnum.OK,
+                    result = "ok"
+                });
+            }
+            catch (Exception err)
+            {
+                return Ok(new ReturnStatus
+                {
+                    status = StatusEnum.ERROR,
+                    result = "error",
+                    error = err.Message
+                });
+            }
         }
     }
 }
---
+++ MyWebAPI_JWT/Model/UserModel.cs
@@ -0,0 +1,21 @@
+﻿namespace MyWebAPI_JWT.Model
+{
+    public class UserModel
+    {
+        public string login { get; set; }
+        public string password { get; set; }
+    }
+
+    public class ReturnStatus
+    {
+        public string result { get; set; }
+        public string error { get; set; }
+        public StatusEnum status { get; set; }
+    }
+
+    public enum StatusEnum
+    {
+        OK = 1,
+        ERROR = 0
+    }
+}
